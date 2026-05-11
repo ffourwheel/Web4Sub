1eb6546e3b35838c9eb69b46785bbc7e6a557202
ffourwheel <punlawat.s@gmail.com>
2026-05-11 20:50:10 +0700

    Update unsupervise

--- models/unsupervise/unsupervise.py
+++ models/unsupervise/unsupervise.py
@@ -10,6 +10,7 @@ from sklearn.mixture import GaussianMixture
 from sklearn.decomposition import PCA
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import TSNE
 from sklearn.metrics import silhouette_score
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -36,7 +37,7 @@ X = pd.concat([skin_type_encoded, concerns_encoded], axis=1).fillna(0)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-pca_clustering = PCA(n_components=0.70, random_state=42)
+pca_clustering = PCA(n_components=2, random_state=42)
 X_pca = pca_clustering.fit_transform(X_scaled)
 
 N_CLUSTERS = 3
